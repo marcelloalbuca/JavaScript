e1dbe5029b603e81b41f653d065ea4a44b452ff9
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-14 00:58:01 -0300

    exemplo replace

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var nome = "Marcello Albuquerque";
 var idade = 32;
-
+var frase = "O Japão é o melhor time do mundo";
 /*
 alert(nome + " tem " + idade + " anos");
 */
 console.log(nome);
+console.log(frase.replace("Japão", "Brasil"));
