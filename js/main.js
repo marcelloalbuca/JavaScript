b7e197280b3a9897fdf729a6b8bbe36a9fa0aa85
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-15 01:22:07 -0300

    functions

--- js/main.js
+++ js/main.js
@@ -66,7 +66,31 @@ console.log(d.getMinutes());
 console.log(d.getDay());
 console.log(d.getHours());
 
+function soma (n1, n2){
+    return n1 + n2;
+}
+console.log(soma(5, 10));
 
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+console.log(setReplace("Vai Japão", "Japão", "Brasil"));
+
+function validaIdade(idade)
+{   
+    var validar;
+    if (idade >= 18)
+    {
+        validar = true;
+    }
+    else
+    {
+        validar = false;
+    }
+    return validar;
+}
+var idade = 19;
+console.log(validaIdade(idade));
 
 
 
