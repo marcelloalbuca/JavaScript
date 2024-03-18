cac029d26e40ab56b9b478d69196e19739fa5d83
marcelloalbuca <marcello.albuca@gmail.com>
2024-03-18 10:20:27 -0300

    functions js dom

---
+++ js/dom-event.js
@@ -0,0 +1,31 @@
+onclick  //quando um objeto é clicado pelo mouse <a>, <input>
+ 
+onselect // quandoum objeto é selecionado <input type=text>, <textarea>
+
+onchange //Quando um objeto é selecionado <input type=text>, <textarea>
+ 
+onfocus // quando um componente de formulário ou janela se torna inativa <textarea>, <body>, <form>, <input>, <select> <option>
+
+onblur //quando um compoente de formulário ou janela se tornar ativa <textarea>, <body>, <form>, <input>, <select> <option>
+
+onmouseover //quando ou mouse esta sobre o link <a>, <area>
+
+onmouseout //quando o mouse deixa um link <a>, <area>
+
+onsubmit //antes de enviar um formulário <input type=submit>
+
+onreset // antes de limpar um formulário <form>
+
+onload //apos carregar uma pagina, janela ou frame <body>
+
+onunload // ao deixar uma página, janela ou frame <body>
+
+onerror //erro ocorre carga imagem ou pagina <img> <body>
+
+onabort // abortar image <img>
+
+
+
+
+
+
