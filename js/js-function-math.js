1862ff39e5be2b64e5bcca69c01bd34813e5ee1d
marcelloalbuca <marcello.albuca@gmail.com>
2024-03-18 12:12:58 -0300

    funcion math

---
+++ js/js-function-math.js
@@ -0,0 +1,39 @@
+acos(x) //cosseno sobre -1
+
+asin(x) //seno sobre -1
+
+atan(x) //tangente  sobre -1
+
+atan2(x, y) //retorna o angulo 0 de um ponto (x, y)
+
+ceil(x) //arredonda pra cima (3.2 e 3.8 -> 4)
+
+cos(x)  //cosseno
+
+exp(x) //x sobre e
+
+floor(x) //arredonda para baixo
+
+random() //retorna um numero pseudo-aleatorio entre 0 e 1
+
+abs(x) //absoluto
+
+max(a,b) //maximo
+
+min(a,b) //minimo
+
+sin(x) //seno
+ 
+pow(a, b) //potencia b sobre a
+
+round() //arredonda 
+
+tan(x) // tangente
+
+sqrt(x) //raiz quadrada
+ 
+log(x)  // logaritmo  natural
+
+
+
+
